343f561c01924b08f2e47f0cbf70f2ca9708c6b8
im6 <ee2win@gmail.com>
2018-06-12 22:34:54 +0800

    remove comment

--- colorpk/colorpk/ViewController.swift
+++ colorpk/colorpk/ViewController.swift
@@ -1,11 +1,3 @@
-//
-//  ViewController.swift
-//  colorpk
-//
-//  Created by Zijian Guo on 8/10/17.
-//  Copyright © 2017 Zijian Guo. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
@@ -14,12 +6,5 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
