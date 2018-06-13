49e0006391e639dc9508c6924aa26b53252ee1f4
im6 <ee2win@gmail.com>
2018-06-13 23:00:34 +0800

    add tabbed app

---
+++ colorpk/colorpk/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  colorpk
+//
+//  Created by Zijian Guo on 6/13/18.
+//  Copyright © 2018 Zijian Guo. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
