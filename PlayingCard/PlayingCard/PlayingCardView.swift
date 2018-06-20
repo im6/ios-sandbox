6ebcc7caf02fa136e80d04078a55aa0494653950
im6 <ee2win@gmail.com>
2018-06-20 23:43:08 +0800

    create a corner rect

--- PlayingCard/PlayingCard/PlayingCardView.swift
+++ PlayingCard/PlayingCard/PlayingCardView.swift
@@ -2,9 +2,20 @@
 import UIKit
 
 class PlayingCardView: UIView {
+    private func centeredAttributedString(_ str: String, fontSize: CGFloat) -> NSAttributedString {
+        var font = UIFont.preferredFont(forTextStyle: .body).withSize(fontSize)
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.alignment = .center
+        return NSAttributedString(string: <#T##String#>)
+        
+        
+    }
+    
     override func draw(_ rect: CGRect) {
-        // Drawing code
-        print("print Here")
+        let roundedRect = UIBezierPath(roundedRect: bounds, cornerRadius: 36)
+        roundedRect.addClip()
+        UIColor.white.setFill()
+        roundedRect.fill()
     }
 
 }
