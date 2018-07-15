9d4ce8e37f65f1fc5cf4dff1b4faf666f5c86b35
im6 <ee2win@gmail.com>
2018-07-15 17:12:25 +0800

    add color example

--- ColorPK/ColorPK/ColorTableViewCell.swift
+++ ColorPK/ColorPK/ColorTableViewCell.swift
@@ -17,6 +17,10 @@ class ColorTableViewCell: UITableViewCell {
             let btnText = currentColor.isLiked ? "❤️" : "♡"
             likedNumLabel?.text = "\(currentColor.likeNum)"
             likeButton.setTitle(btnText, for: UIControlState.normal)
+            colorButtons[0].backgroundColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
+            colorButtons[1].backgroundColor = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)
+            colorButtons[2].backgroundColor = #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)
+            colorButtons[3].backgroundColor = #colorLiteral(red: 0.9686274529, green: 0.78039217, blue: 0.3450980484, alpha: 1)
         }
         
     }
