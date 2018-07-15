e7c244f2c89e4c028bc04873df62f82fb23ec79e
im6 <ee2win@gmail.com>
2018-07-15 22:09:06 +0800

    bind with real value

--- ColorPK/ColorPK/ColorTableViewCell.swift
+++ ColorPK/ColorPK/ColorTableViewCell.swift
@@ -11,23 +11,34 @@ class ColorTableViewCell: UITableViewCell {
         }
     }
     
+    private func convertStringToColor(str: String) -> UIColor? {
+        let slice0 = str[str.startIndex..<str.index(str.startIndex, offsetBy: 2)]
+        let slice1 = str[str.index(str.startIndex, offsetBy: 2)..<str.index(str.startIndex, offsetBy: 4)]
+        let slice2 = str[str.index(str.startIndex, offsetBy: 4)..<str.index(str.startIndex, offsetBy: 6)]
+        if let hexValue0 = UInt(slice0, radix: 16),
+            let hexValue1 = UInt(slice1, radix: 16),
+            let hexValue2 = UInt(slice2, radix: 16) {
+            let colorResult = UIColor(red: CGFloat(Float(hexValue0)/255.0),
+                                 green: CGFloat(Float(hexValue1)/255.0),
+                                 blue: CGFloat(Float(hexValue2)/255.0),
+                                 alpha: 1)
+            return colorResult
+        } else {
+            return nil
+        }
+    
+    }
+    
     private func updateUI() {
         if let currentColor = color {
             let btnHeart = currentColor.isLiked ? "👍" : "👍"
             let btnText = "\(btnHeart) \(currentColor.likeNum)"
             likeButton.setTitle(btnText, for: UIControlState.normal)
-            colorButtons[0].backgroundColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
-            colorButtons[1].backgroundColor = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)
-            colorButtons[2].backgroundColor = #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)
+            colorButtons[0].backgroundColor = convertStringToColor(str: currentColor.color[0])
+            colorButtons[1].backgroundColor = convertStringToColor(str: currentColor.color[1])
+            colorButtons[2].backgroundColor = convertStringToColor(str: currentColor.color[2])
+            colorButtons[3].backgroundColor = convertStringToColor(str: currentColor.color[3])
             
-            if let str = color?.color[0]{
-                let slice0 = str[str.startIndex..<str.index(str.startIndex, offsetBy: 2)]
-                let slice1 = str[str.index(str.startIndex, offsetBy: 2)..<str.index(str.startIndex, offsetBy: 4)]
-                let slice2 = str[str.index(str.startIndex, offsetBy: 4)..<str.index(str.startIndex, offsetBy: 6)]
-                print("\(slice0), \(slice1), \(slice2)")
-            }
-            
-        
         }
         
     }
