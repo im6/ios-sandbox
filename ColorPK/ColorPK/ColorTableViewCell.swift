1d5910d8be28e2f3acf9c2fada6fa6f25a8f3236
im6 <ee2win@gmail.com>
2018-07-21 13:43:07 +0800

    create singleton for shared resource

--- ColorPK/ColorPK/ColorTableViewCell.swift
+++ ColorPK/ColorPK/ColorTableViewCell.swift
@@ -19,32 +19,18 @@ class ColorTableViewCell: UITableViewCell {
         }
     }
     
-    private func convertStringToColor(str: String) -> UIColor? {
-        let slice0 = str[str.startIndex..<str.index(str.startIndex, offsetBy: 2)]
-        let slice1 = str[str.index(str.startIndex, offsetBy: 2)..<str.index(str.startIndex, offsetBy: 4)]
-        let slice2 = str[str.index(str.startIndex, offsetBy: 4)..<str.index(str.startIndex, offsetBy: 6)]
-        if let hexValue0 = UInt(slice0, radix: 16),
-            let hexValue1 = UInt(slice1, radix: 16),
-            let hexValue2 = UInt(slice2, radix: 16) {
-            let colorResult = UIColor(red: CGFloat(Float(hexValue0)/255.0),
-                                 green: CGFloat(Float(hexValue1)/255.0),
-                                 blue: CGFloat(Float(hexValue2)/255.0),
-                                 alpha: 1)
-            return colorResult
-        } else {
-            return nil
-        }
-    }
-    
     private func updateUI() {
+        
+        let convFn = Shared.sharedInstance.convertStringToColor
+        
         if let currentColor = color {
             let btnHeart = currentColor.isLiked ? "♥️" : "♡"
             let btnText = "\(btnHeart) \(currentColor.likeNum)"
             likeButton.setTitle(btnText, for: UIControlState.normal)
-            colorButtons[0].backgroundColor = convertStringToColor(str: currentColor.color[0])
-            colorButtons[1].backgroundColor = convertStringToColor(str: currentColor.color[1])
-            colorButtons[2].backgroundColor = convertStringToColor(str: currentColor.color[2])
-            colorButtons[3].backgroundColor = convertStringToColor(str: currentColor.color[3])
+            colorButtons[0].backgroundColor = convFn(currentColor.color[0])
+            colorButtons[1].backgroundColor = convFn(currentColor.color[1])
+            colorButtons[2].backgroundColor = convFn(currentColor.color[2])
+            colorButtons[3].backgroundColor = convFn(currentColor.color[3])
         }
         
 //        let bgColorView = UIView()
---
+++ ColorPK/ColorPK/Shared.swift
@@ -0,0 +1,28 @@
+
+import Foundation
+import UIKit
+
+class Shared {
+    static let sharedInstance = Shared()
+    
+    func convertStringToColor(str: String) -> UIColor? {
+        let slice0 = str[str.startIndex..<str.index(str.startIndex, offsetBy: 2)]
+        let slice1 = str[str.index(str.startIndex, offsetBy: 2)..<str.index(str.startIndex, offsetBy: 4)]
+        let slice2 = str[str.index(str.startIndex, offsetBy: 4)..<str.index(str.startIndex, offsetBy: 6)]
+        if let hexValue0 = UInt(slice0, radix: 16),
+            let hexValue1 = UInt(slice1, radix: 16),
+            let hexValue2 = UInt(slice2, radix: 16) {
+            let colorResult = UIColor(red: CGFloat(Float(hexValue0)/255.0),
+                                      green: CGFloat(Float(hexValue1)/255.0),
+                                      blue: CGFloat(Float(hexValue2)/255.0),
+                                      alpha: 1)
+            return colorResult
+        } else {
+            return nil
+        }
+    }
+    
+    private init() {
+        // initialize property
+    }
+}
